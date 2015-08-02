f57a03ba2fca727f00e820da5ab965c8f4b35049
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-02 20:43:31 +0200

    Update ClojureScript to 1.7.28

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/clojurescript "1.7.28"]
                  [com.cognitect/transit-clj "0.8.275"
                   :exclusions
                   [com.fasterxml.jackson.datatype/jackson-datatype-json-org
